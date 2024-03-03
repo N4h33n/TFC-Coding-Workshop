f5b5b6a6708fafbabfa462a226d81121ef3111b5
Naheen <92299515+N4h33n@users.noreply.github.com>
2024-03-03 07:23:11 -0700

    Add files via upload

---
+++ pyscript.py
@@ -0,0 +1,36 @@
+# BASIC DATA TYPES
+
+# numeric
+
+# text
+
+# boolean
+
+
+# STORING DATA
+
+# variables vs constants
+
+#arithmetic operations
+
+# arithmetic operations on strings?
+
+# COMPLEX DATA TYPES
+
+# lists
+
+# tuples
+
+# dictionaries
+
+# sets
+
+# conditions
+
+# loops
+
+#while
+
+#for
+
+# functions
